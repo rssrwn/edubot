37d963a9b45a96203335fefb1233499be10f07fc
ri616 <ri616@ic.ac.uk>
2018-05-30 13:42:24 +0100

    added initial local django site

---
+++ backend-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/homes/ri616/Documents/Programming/WebApps/g1727114/backend-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
