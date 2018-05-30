0bf7ea13a41774a520652fb0795c75e9df78bdcd
Jordan Glanfield <jg1216@voxel09.doc.ic.ac.uk>
2018-05-30 15:53:31 +0100

    [jg1216] Added ability to rotate robot, needs testing.

--- web/scripts/robot_control.js
+++ web/scripts/robot_control.js
@@ -5,15 +5,27 @@ var Dir = {
   UP: 3,
   
   properties: {
-    0: {moveForward: function(point) {return new Point(point.x + 1, point.y)}},
-    1: {moveForward: function(point) {return new Point(point.x, point.y + 1)}},
-    2: {moveForward: function(point) {return new Point(point.x - 1, point.y)}},
-    3: {moveForward: function(point) {return new Point(point.x, point.y - 1)}}
-  }
+    0: {moveForward: function(point) {return new Point(point.x + 1, point.y)},
+        rotateRight: DOWN, rotateLeft: UP},
+    1: {moveForward: function(point) {return new Point(point.x, point.y + 1)},
+        rotateRight: LEFT, rotateLeft: RIGHT},
+    2: {moveForward: function(point) {return new Point(point.x - 1, point.y)},
+        rotateRight: UP, rotateLeft: DOWN},
+    3: {moveForward: function(point) {return new Point(point.x, point.y - 1)},
+        rotateRight: RIGHT, rotateLeft: LEFT}
+  };
 };
 
 Robot.prototype.dir = Dir.RIGHT;
 
 Robot.prototype.moveForward = function() {
-  
+  this.loc = Dir.properties[this.dir].moveForward(this.loc);
+}
+
+Robot.prototype.rotateRight = function() {
+  this.dir = Dir.properties[this.dir].rotateRight;
+}
+
+Robot.prototype.rotateLeft = function() {
+  this.dir = Dir.properties[this.dir].rotateLeft;
 }
