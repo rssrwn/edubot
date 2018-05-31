a4f46363e162d1aeb652c11f5778b1af51ebdf12
Harry Davis <harry.davis16@imperial.ac.uk>
2018-05-31 15:56:13 +0100

    [hrd16][jg1216] Centered canvas for smaller levels

--- custom_blocks/robot_generator.js
+++ custom_blocks/robot_generator.js
@@ -1,17 +1,17 @@
 Blockly.JavaScript['move_forward'] = function(block) {
-  var code = 'edubot.moveForward();\nsleep();\n';
+  var code = 'edubot.moveForward();\nawait sleep(robotTimeStep);\n';
   return code;
 };
 
 Blockly.JavaScript['rotate_right'] = function(block) {
   var dropdown_angle = block.getFieldValue('angle');
-  var code = 'edubot.rotateRight();\nsleep();\n';
+  var code = 'edubot.rotateRight();\nawait sleep(robotTimeStep);\n';
   return code;
 };
 
 Blockly.JavaScript['rotate_left'] = function(block) {
   var dropdown_angle = block.getFieldValue('angle');
-  var code = 'edubot.rotateLeft();\nsleep();\n';
+  var code = 'edubot.rotateLeft();\nawait sleep(robotTimeStep);\n';
   return code;
 };
 
--- web/scripts/edubot.js
+++ web/scripts/edubot.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("game_canvas");
 var ctx = canvas.getContext("2d");
-var level = new GridLevel(16, 16, 32);
+var level = new GridLevel(24, 24, 32);
 var edubot = new Robot();
 var robotStepTime = 400;
 
@@ -30,13 +30,10 @@ function update() {
 
 // Repaints the game
 function draw() {
-  let scale = level.width / 32;
-  
-  let size = 1024 - level.width * level.squareSize;
-  
-  ctx.translate(size / 2 * scale, size / 2 * scale);
-  
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  let offset = (canvas.width - level.width * level.squareSize) / 2;
+  ctx.translate(offset, offset);
   
   level.grid.forEach(function (column) {
     column.forEach(function (square) {
