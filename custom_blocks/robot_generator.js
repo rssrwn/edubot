6fb23c193f8aee222513ecf8c8a97aa66f7bf2c7
Jordan Glanfield <jg1216@sprite05.doc.ic.ac.uk>
2018-05-31 14:57:00 +0100

    [jg1216][hrd16] Implemented and tested command stack and prevented multiple command executions from running at once.

--- custom_blocks/robot_generator.js
+++ custom_blocks/robot_generator.js
@@ -1,16 +1,16 @@
 Blockly.JavaScript['move_forward'] = function(block) {
-  var code = 'edubot.moveForward();\n';
+  var code = 'addCommand(new MoveCommand());\n';
   return code;
 };
 
 Blockly.JavaScript['rotate_right'] = function(block) {
   var dropdown_angle = block.getFieldValue('angle');
-  var code = 'edubot.rotateRight(' + ');\n';
+  var code = 'addCommand(new RotateRightCommand());\n';
   return code;
 };
 
 Blockly.JavaScript['rotate_left'] = function(block) {
   var dropdown_angle = block.getFieldValue('angle');
-  var code = 'edubot.rotateLeft(' + ');\n';
+  var code = 'addCommand(new RotateLeftCommand());\n';
   return code;
 };
