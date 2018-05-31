f2f469b4560da04d7748558aeee0169518cb6879
Harry Davis <harry.davis16@imperial.ac.uk>
2018-05-31 20:36:26 +0100

    [hrd16] Added CORS for backend

--- backend_js/index.js
+++ backend_js/index.js
@@ -21,6 +21,20 @@ const pool = new Pool({
   pool.end()
 })*/
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
+
 app.get('/', (req, res, next) => {
   res.send('You reached the home page');
 });
