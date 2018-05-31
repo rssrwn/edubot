e7826ede4897eb0a729911d2fe27454353edcf74
Harry Davis <harry.davis16@imperial.ac.uk>
2018-05-31 16:05:16 +0100

    [hrd16][jg1216] runCode now works

--- custom_blocks/robot_generator.js
+++ custom_blocks/robot_generator.js
@@ -1,17 +1,17 @@
 Blockly.JavaScript['move_forward'] = function(block) {
-  var code = 'edubot.moveForward();\nawait sleep(robotTimeStep);\n';
+  var code = 'edubot.moveForward();\nawait sleep(robotStepTime);\n';
   return code;
 };
 
 Blockly.JavaScript['rotate_right'] = function(block) {
   var dropdown_angle = block.getFieldValue('angle');
-  var code = 'edubot.rotateRight();\nawait sleep(robotTimeStep);\n';
+  var code = 'edubot.rotateRight();\nawait sleep(robotStepTime);\n';
   return code;
 };
 
 Blockly.JavaScript['rotate_left'] = function(block) {
   var dropdown_angle = block.getFieldValue('angle');
-  var code = 'edubot.rotateLeft();\nawait sleep(robotTimeStep);\n';
+  var code = 'edubot.rotateLeft();\nawait sleep(robotStepTime);\n';
   return code;
 };
 
