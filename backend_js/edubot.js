e83b32e96017efe05cbdd9e8645aee61d37d48b9
ri616 <ri616@ic.ac.uk>
2018-06-04 23:58:50 +0100

    all paths to /teacher are blocked by auth middleware'

--- backend_js/edubot.js
+++ backend_js/edubot.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-//const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 // Third-party middleware
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Our middleware
-app.use(require('./controllers'));
+// Our middleware (top executed first)
 app.use(require('./middlewares/res_header'));
+app.use('/teacher', require('./middlewares/auth'));
+app.use(require('./controllers'));
 
+// Listen on port
 app.listen(PORT, () => {
   console.log(`EduBot is running on port ${ PORT }`);
 });
