5a6748a4aaa6856b76b2a5773cf542174b4c01a9
ri616 <ri616@ic.ac.uk>
2018-06-05 17:32:03 +0100

    initial route for classes

--- backend_js/controllers/teacher.js
+++ backend_js/controllers/teacher.js
@@ -11,7 +11,13 @@ router.get('/student', (req, res, next) => {
 });
 
 router.get('/level_selection', (req, res, next) => {
-
+  var context = {
+    class_list: [{
+      class_name: 'class1',
+      class_members: ['Harry', 'Ross', 'Jordan', 'Julien']
+    }]
+  };
+  res.render('teacher/classes', context);
 });
 
 router.get('/account', (req, res, next) => {
