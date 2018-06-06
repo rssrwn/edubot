901e95617a30a5d4b182c6d46f85499414304a7e
Harry Davis <harry.davis16@imperial.ac.uk>
2018-06-06 11:40:43 +0100

    [hrd16] Added category partials to select template

--- backend_js/controllers/student.js
+++ backend_js/controllers/student.js
@@ -5,21 +5,27 @@ const db = require('../models/db.js');
 router.get('/level_selection', (req, res, next) => {
   var context = {
     student: true,
-    levels: [
-    {
-      number: 1,
-      score0: false,
-      score1: true,
-      score2: false,
-      score3: false
-    },
-    {
-      number: 2,
-      score0: true,
-      score1: false,
-      score2: false,
-      score3: false
-    }]
+    categorys: [
+      {
+        categoryName: "Introduction",
+        levels: [
+          {
+            number: 1,
+            score0: false,
+            score1: true,
+            score2: false,
+            score3: false
+          },
+          {
+            number: 2,
+            score0: true,
+            score1: false,
+            score2: false,
+            score3: false
+          }
+        ]
+      }
+    ]
   };
   res.render('student/level_select', context);
 });
