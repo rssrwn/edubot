4e33b699b6342ee1c5e3afac427ef8dfb9dff0b4
ri616 <ri616@ic.ac.uk>
2018-06-06 18:53:17 +0100

    turned off auth

--- backend_js/edubot.js
+++ backend_js/edubot.js
@@ -35,8 +35,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Our middleware (top executed first)
 app.use(require('./middlewares/res_header'));
-app.use('/teacher', require('./middlewares/teacher_auth'));
-app.use('/student', require('./middlewares/student_auth'));
+//app.use('/teacher', require('./middlewares/teacher_auth'));
+//app.use('/student', require('./middlewares/student_auth'));
 app.use(require('./controllers'));
 
 // Listen on port
