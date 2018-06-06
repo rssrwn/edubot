bbe48d20814b6753e0a47a35318449a9cefac8d3
ja1616 <ja1616@ic.ac.uk>
2018-06-06 15:09:21 +0100

    [ri616][ja1616] Fixed syntax bug with student class list info access

--- backend_js/controllers/teacher.js
+++ backend_js/controllers/teacher.js
@@ -3,25 +3,19 @@ const router = express.Router();
 const db = require('../models/db.js');
 
 router.get('/classes', (req, res, next) => {
-  // var context = {
-  //   student: false,
-  //   class_list: [{
-  //     class_name: 'Group 14',
-  //     class_members: [{ fname: 'Harry', lname: 'Davis', age: 20, uname: 16},
-  //                     { fname: 'Ross', lname: 'Irwin', age: 20, uname: 616},
-  //                     { fname: 'Jordan', lname: 'Glanfield', age: 20, uname: 1216},
-  //                     { fname: 'Julien', lname: 'Amblard', age: 19, uname: 1616}]
-  //   }, {
-  //     class_name: 'Older Members',
-  //     class_members: [{ fname: 'Mark', lname: 'Wheelhouse', age: 0, uname: 322}]
-  //   }]
-  // };
   var context = {
     student: false,
     class_list: [{
-      class_name: 'class1',
-      class_members: ['Harry', 'Ross', 'Jordan', 'Julien']
+      class_name: 'Group 14',
+      class_members: [{ fname: 'Harry', lname: 'Davis', age: 20, uname: 16},
+                      { fname: 'Ross', lname: 'Irwin', age: 20, uname: 616},
+                      { fname: 'Jordan', lname: 'Glanfield', age: 20, uname: 1216},
+                      { fname: 'Julien', lname: 'Amblard', age: 19, uname: 1616}]
+    }, {
+      class_name: 'Older Members',
+      class_members: [{ fname: 'Mark', lname: 'Wheelhouse', age: 0, uname: 322}]
     }]
+  };
   };
 
   res.render('teacher/classes', context);
