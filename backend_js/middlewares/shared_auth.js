22fa9bfe8fa3b8a28c504da281f8f00f6af0fdcf
ri616 <ri616@ic.ac.uk>
2018-06-07 21:48:58 +0100

    testing

--- backend_js/middlewares/shared_auth.js
+++ backend_js/middlewares/shared_auth.js
@@ -3,6 +3,7 @@ const db = require('../models/db.js');
 module.exports = async function(req, res, next) {
   // console.log("student cookies auth", req.cookies);
   uname = req.cookies['edubot-cookie'];
+  console.log('uname: ', uname);
   var type = await db.getUserType(uname);
 
   if (type === db.userTypeEnum.STUDENT || type === db.userTypeEnum.TEACHER) {
