96ea31ede9a88c5a31c472a16f905efb71bffe0e
Jordan Glanfield <jg1216@matrix28.doc.ic.ac.uk>
2018-06-07 12:17:07 +0100

    [jg1216] Fixed crashing error.

--- backend_js/controllers/shared.js
+++ backend_js/controllers/shared.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db.js');
-const fs = require('fs');
+/*const fs = require('fs');
 
 function getLevelIntroContext(levelName, callback) {
   fs.readFile('../shared/levels/' + levelName + '/' + levelName + '.ctx', 'utf8', function (err, data) {
@@ -18,9 +18,9 @@ function getLevelIntroContext(levelName, callback) {
 
 var introContext = null;
 
-getLevelIntroContext("intro_1", function (ctx) {introContext = ctx});
+getLevelIntroContext("intro_1", function (ctx) {introContext = ctx});*/
 
-/*var introContext = {
+var introContext = {
   student: false,
   concept: "Loops and Conditionals",
   concept_description: ["So far we have covered simple instructions such as move forward, turn left and turn right, using these to feed poor edubot.", "However, when the environment becomes larger or more complicated, manually telling edubot what to do will become too long.", "A level with 20 bolts to eat will require at minimum 20 code blocks!", "This is where loops and conditionals come in. A loop lets edubot repeat an action multiple times.", "It may repeat a fixed number of times or perhaps repeat until edubot hits a wall. By taking advantage of patterns in the level, we can code edubot using far fewer instructions."],
@@ -34,7 +34,7 @@ getLevelIntroContext("intro_1", function (ctx) {introContext = ctx});
   "If you're stuck, you can start by manually guiding the robot around the level and seeing what patterns appear in the actions, and what can be placed in a loop."],
   
   level_image: "../images/loops_and_iteration_1.png"
-};*/
+};
 
 router.get('/level_intro', (req, res, next) => {
   res.render('shared/level_intro', introContext);
