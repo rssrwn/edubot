8bddc6a649065285cf3f1aa83a28faa8fe01c10e
ri616 <ri616@ic.ac.uk>
2018-06-10 23:58:33 +0100

    minor ui touches

--- backend_js/controllers/index.js
+++ backend_js/controllers/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const db = require('../models/db.js');
 const router = express.Router();
 
@@ -8,13 +9,17 @@ router.use('/teacher', require('./teacher'));
 router.use('/student', require('./student'));
 router.use('/shared', require('./shared'));
 
-router.get('/test', async function(req, res) {
+router.get('/test', async function(req, res, next) {
   //var info = await db.insertClass('Year 8C', 1, 'teacher2');
   //var info = await db.addMember('ryan06', 1);
   //var info = await db.getClassName(1);
   let info = await db.getLevelResults('user2');
   console.log(info);
   res.sendStatus(200);
+});
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname + './../public/login.html'));
 });
 
 module.exports = router;
