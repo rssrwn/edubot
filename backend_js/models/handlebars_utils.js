e75433a31f9c5fbcc782016e4249039d1cc4045c
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2018-06-11 13:07:37 +0100

    [jg1216] Added further logging for debugging purposes for for loops.

--- backend_js/models/handlebars_utils.js
+++ backend_js/models/handlebars_utils.js
@@ -1,6 +1,8 @@
 exports.forFunction = function(from, to, increment, elem) {
   let code = "";
+  console.log("Iterating through for loop: from " + from + " to " + to + " with increment " + increment);
   for (let i = from; i < to; i += increment) {
+    console.log("Running iteration: i = " + i + ", to = " + to + ", inc = " + increment);
     code += elem.fn(i);
   }
   return code;
