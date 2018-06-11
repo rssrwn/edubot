03c84931d1aee532414d3e79f6a24379502e13f7
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2018-06-11 13:18:05 +0100

    [jg1216][hrd16] Added further logging and attempted to address page resizing issues.

--- backend_js/models/handlebars_utils.js
+++ backend_js/models/handlebars_utils.js
@@ -1,10 +1,16 @@
 exports.forFunction = function(from, to, increment, elem) {
   let code = "";
   console.log("Iterating through for loop: from " + from + " to " + to + " with increment " + increment);
-  for (let i = from; i < to; i += increment) {
+  
+  let i = from;
+  
+  for (i = from; i < to; i += increment) {
     console.log("Running iteration: i = " + i + ", to = " + to + ", inc = " + increment);
     code += elem.fn(i);
     console.log("Completed an iteration: i = " + i + ", to = " + to + ", inc = " + increment);
   }
+  
+  console.log("Finished for loop: i = " + i + ", to = " + to + ", inc = " + increment);
+  
   return code;
 }
