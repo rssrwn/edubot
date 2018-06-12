0d04061fc365103cb0415d441a1f09a53395811a
Harry Davis <harry.davis16@imperial.ac.uk>
2018-06-12 12:19:00 +0100

    [hrd16] Fix tutorial block disabling

--- backend_js/public/scripts/tutorial.js
+++ backend_js/public/scripts/tutorial.js
@@ -32,7 +32,6 @@ var updateTutorial = function(e) {
   }
   if (tutorialPhase == 5) {
     workspace.options.maxBlocks = 1000;
-    blocklyResize();
   }
 
   if (text[tutorialPhase] !== undefined) {
