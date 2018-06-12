9de0f0e19d0d7904d97a5ea93b4b345438b6e85d
ja1616 <ja1616@ic.ac.uk>
2018-06-12 11:49:25 +0100

    [ja1616] Fixed issue loading uncompleted levels on student page

--- backend_js/controllers/teacher.js
+++ backend_js/controllers/teacher.js
@@ -35,6 +35,7 @@ router.get('/student', async function(req, res, next) {
   let studentId = req.query.userId;
   let studentInfo = await db.getUserInfo(studentId);
   let results = await db.getLevelResults(studentId);
+  let currLevel = await db.getCurrLevel(studentId);
   
   var categories = [
     {
