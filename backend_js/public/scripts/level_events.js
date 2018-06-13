96c340bac7fa751a2078d9509f7c0e14a32892c4
ja1616 <ja1616@ic.ac.uk>
2018-06-13 11:32:31 +0100

    [ja1616] Added initial feedback interface for teachers

--- backend_js/public/scripts/level_events.js
+++ backend_js/public/scripts/level_events.js
@@ -70,29 +70,26 @@ function restart(e) {
 document.getElementById("restartButton").addEventListener("click", restart);
 document.getElementById("simpleRestartButton").addEventListener("click", restart);
 
-var feedbackActiveButton = null;
+function giveFeedback(feedbackButton) {
+  feedbackButton.style.display = "none";
 
-function giveFeedback(button) {
-  if (feedbackActiveButton) {
-    $(feedbackActiveButton.addButton).remove();
-    $(feedbackActiveButton.textInput).remove();
-    feedbackActiveButton.css("display") = "inline-block";
-  }
-  feedbackActiveButton = button;
+  var submitButton = $("<button type='button' id='submitFeedbackButton' onclick='submitFeedback(feedbackButton)'>Give Feedback</button>");
+  var textInput = $("<input type='text' class='submitFeedbackInput'></input>");
 
-  button.css("display") = "none";
-
-  var submitButton = $("<button type='button' onclick='addClassSubmitEvent()'>Give Feedback</button>");
-  var textInput = $("<input type='text'></input>");
-
-  feedbackActiveButton.addButton = submitButton.get(0);
-  feedbackActiveButton.textInput = textInput.get(0);
-
-  $(button).parent().append(textInput);
-  $(button).parent().append(submitButton);
+  $(feedbackButton).parent().append(textInput);
+  $(feedbackButton).parent().append(submitButton);
 }
 
 document.getElementById("simpleFeedbackButton").addEventListener("click", giveFeedback);
+
+function submitFeedback(feedbackButton) {
+  feedbackButton.style.display = "";
+  
+  console.log($("#submitFeedbackInput").text());
+  
+  $("#submitFeedbackButton").remove();
+  $("#submitFeedbackInput").remove();
+}
 
 var blocklyResize = function(e) {
   let element = blocklyDiv;
