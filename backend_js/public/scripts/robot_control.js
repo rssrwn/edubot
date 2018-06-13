2070a6107d6b7f88b5e64bb3455ac49169d34a78
Harry Davis <harry.davis16@imperial.ac.uk>
2018-06-13 12:30:56 +0100

    [hrd16] Log

--- backend_js/public/scripts/robot_control.js
+++ backend_js/public/scripts/robot_control.js
@@ -127,6 +127,7 @@ Robot.prototype.removed = function () {
 }
 
 Robot.prototype.loaded = function () {
+  console.log("Robot loaded");
   this.anims = new RobotAnims();
   this.prevLoc = new Point(this.loc.x, this.loc.y);
   this.curDir = 0;
