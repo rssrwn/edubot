74ed9514d5b6027b4ff4d024357ff226ec324f1f
Harry Davis <harry.davis16@imperial.ac.uk>
2018-06-13 12:59:23 +0100

    [hrd16] Blocks counted in scoring

--- backend_js/public/scripts/robot_control.js
+++ backend_js/public/scripts/robot_control.js
@@ -127,7 +127,6 @@ Robot.prototype.removed = function () {
 }
 
 Robot.prototype.loaded = function () {
-  console.log("Robot loaded " + this.loc);
   this.anims = new RobotAnims();
   this.prevLoc = new Point(this.loc.x, this.loc.y);
   this.curDir = 0;
