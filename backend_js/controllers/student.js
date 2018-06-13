3564e964c048235309520693ca06fcc7fb0fea26
Jordan Glanfield <jg1216@matrix38.doc.ic.ac.uk>
2018-06-13 11:25:05 +0100

    [jg1216] Fixed paths for level_selection once more.

--- backend_js/controllers/student.js
+++ backend_js/controllers/student.js
@@ -50,7 +50,7 @@ router.get('/level_selection', async function(req, res, next) {
     currLevel: currLevel
   };
 
-  res.render('/shared/level_selection', context);
+  res.render('shared/level_selection', context);
 });
 
 router.get('/level_results', (req, res, next) => {
