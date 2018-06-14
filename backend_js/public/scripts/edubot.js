845b174b158978e717adbf0fd924b645826f66c9
Harry Davis <harry.davis16@imperial.ac.uk>
2018-06-14 16:06:14 +0100

    [hrd16] Added score logging

--- backend_js/public/scripts/edubot.js
+++ backend_js/public/scripts/edubot.js
@@ -142,16 +142,16 @@ GridLevel.prototype.levelCompleted = async function() {
   let rob = getRobot();
 
   if (rob !== null) {
-    //console.log("Actions Taken: " + rob.actionsTaken);
+    console.log("Actions Taken: " + rob.actionsTaken);
     let actionDiff = this.maxActions - this.minActions;
     let actionScore = Math.max(actionDiff - (rob.actionsTaken - this.minActions), 0) / actionDiff;
-    //console.log("Action Score: " + actionScore);
+    console.log("Action Score: " + actionScore);
     
     let blocksUsed = countMoveBlocks();
-    //console.log("Move Blocks Used: " + blocksUsed);
+    console.log("Move Blocks Used: " + blocksUsed);
     let blockDiff = this.maxBlocks - this.minBlocks;
     let blockScore = Math.max(blockDiff - (blocksUsed - this.minBlocks), 0) / blockDiff;
-    //console.log("Block Score: " + blockScore);
+    console.log("Block Score: " + blockScore);
     
     let score = 0.3 * actionScore + 0.7 * blockScore;
     starsAttained = Math.min(Math.floor(score * 3) + 1, 3);
